80a913bb0167c56137e0a7519586f3c7c50882eb
Denis Martinevski <DenisMartinevski@gmail.com>
2020-03-02 19:27:56 +0300

    Finished Book model

--- BookSpace/src/by/epam/BookSpace/model/Book.java
+++ BookSpace/src/by/epam/BookSpace/model/Book.java
@@ -23,4 +23,83 @@ public class Book {
         this.discription = "";
         this.id = 0;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAuthorID() {
+        return authorID;
+    }
+
+    public void setAuthorID(int authorID) {
+        this.authorID = authorID;
+    }
+
+    public int getSeriesID() {
+        return seriesID;
+    }
+
+    public void setSeriesID(int seriesID) {
+        this.seriesID = seriesID;
+    }
+
+    public ArrayList<String> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(ArrayList<String> genres) {
+        this.genres = new ArrayList<String>(genres);
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = new Date(releaseDate.toString());
+    }
+
+    public int getCntSymbols() {
+        return cntSymbols;
+    }
+
+    public void setCntSymbols(int cntSymbols) {
+        this.cntSymbols = cntSymbols;
+    }
+
+    public String getDiscription() {
+        return discription;
+    }
+
+    public void setDiscription(String discription) {
+        this.discription = discription;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Book{ ");
+        sb.append("name=").append(name);
+        sb.append(", authorID=").append(authorID);
+        sb.append(", seriesID=").append(seriesID);
+        sb.append(", genres=").append(genres);
+        sb.append(", releaseDate=").append(releaseDate);
+        sb.append(", cntSymbols=").append(cntSymbols);
+        sb.append(", discription=").append(discription);
+        sb.append(", id=").append(id);
+        sb.append(" }");
+        return sb.toString();
+    }
 }
