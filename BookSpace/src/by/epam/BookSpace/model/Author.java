88f62f268c3b69c75a9b1f47ad14663335f84439
Ignat Fomin <zubastiysteam@gmail.com>
2020-03-02 19:29:02 +0300

    Add Author Update

--- BookSpace/src/by/epam/BookSpace/model/Author.java
+++ BookSpace/src/by/epam/BookSpace/model/Author.java
@@ -13,6 +13,57 @@ public class Author {
         this.rating = 0;
         this.description = "";
         this.id = 0;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Author{ ");
+        sb.append("name= ").append(name).append('\'');
+        sb.append(", surname= ").append(surname).append('\'');
+        sb.append(", rating= ").append(rating);
+        sb.append(", description= ").append(description).append('\'');
+        sb.append(", id= ").append(id);
+        sb.append('}');
+        return sb.toString();
     }
 }
