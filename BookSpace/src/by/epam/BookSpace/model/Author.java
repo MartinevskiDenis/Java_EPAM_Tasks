d583d2f7dffe6a34c8abe91da36b58db4c9659e5
Ignat Fomin <zubastiysteam@gmail.com>
2020-03-02 19:21:20 +0300

    Add author model

---
+++ BookSpace/src/by/epam/BookSpace/model/Author.java
@@ -0,0 +1,18 @@
+package by.epam.BookSpace.model;
+
+public class Author {
+    private String name;
+    private String surname;
+    private int rating;
+    private  String description;
+    private int id;
+
+    public Author() {
+        this.name = "";
+        this.surname = "";
+        this.rating = 0;
+        this.description = "";
+        this.id = 0;
+
+    }
+}
