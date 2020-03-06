a19ad7ea1e3d2d8ad7127f9f8a4fead43b651ac1
Ignat Fomin <zubastiysteam@gmail.com>
2020-03-06 13:15:30 +0300

    update comments

--- BookSpace/src/by/epam/BookSpace/model/Comment.java
+++ BookSpace/src/by/epam/BookSpace/model/Comment.java
@@ -6,11 +6,13 @@ public class Comment {
     private String userLogin;
     private String text;
     private int bookID;
+    private int id;
 
     public Comment() {
         this.userLogin = "";
         this.text = "";
         this.bookID = 0;
+        this.id = 0;
     }
 
     public String getUserLogin() {
@@ -37,12 +39,21 @@ public class Comment {
         this.bookID = bookID;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("Comment{ ");
         sb.append("userLogin=").append(userLogin);
         sb.append(", text=").append(text);
         sb.append(", bookID=").append(bookID);
+        sb.append(", id=").append(id);
         sb.append(" }");
         return sb.toString();
     }
@@ -53,12 +64,13 @@ public class Comment {
         if (o == null || getClass() != o.getClass()) return false;
         Comment comment = (Comment) o;
         return bookID == comment.bookID &&
+                id == comment.id &&
                 userLogin.equals(comment.userLogin) &&
                 text.equals(comment.text);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(userLogin, text, bookID);
+        return Objects.hash(userLogin, text, bookID, id);
     }
 }
