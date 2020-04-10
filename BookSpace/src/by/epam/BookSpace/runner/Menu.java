2299c3b2f0693d25e1b59f55ef9986d7eb2e12d9
Denis Martinevski <denismartinevski@gmail.com>
2020-04-10 16:18:59 +0300

    Add begin of menu

---
+++ BookSpace/src/by/epam/BookSpace/runner/Menu.java
@@ -0,0 +1,12 @@
+package by.epam.BookSpace.runner;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Menu {
+    private static final Logger log = LogManager.getLogger();
+
+    public void start() {
+
+    }
+}
